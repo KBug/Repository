89cf995d0a552086e9944a4770bdd1f03bd62e9f
ParrotDevelopers <72994504+ParrotDevelopers@users.noreply.github.com>
2022-01-29 18:33:58 +0100

    fdhg

---
+++ script.module.js2py/lib/js2py/internals/constructors/jsconsole.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from ..conversions import *
+from ..func_utils import *
+
+class ConsoleMethods:
+    def log(this, args):
+        x = ' '.join(to_string(e) for e in args)
+        print(x)
+        return undefined
