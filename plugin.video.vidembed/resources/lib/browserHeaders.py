e06ab0cc88ddd326caf0081ff8abf143f39082f8
ParrotDevelopers <72994504+ParrotDevelopers@users.noreply.github.com>
2022-01-30 20:09:05 +0100

    Update

---
+++ plugin.video.vidembed/resources/lib/browserHeaders.py
@@ -0,0 +1,14 @@
+# Copyright © 2022 Parrot Developers.
+# Coded by Parrot Developers alias HereIronman7746.
+# Do not share this addon.
+import base64,codecs,requests
+try:
+    magic = 'aGVhZGVycyA9IHsNCiAgICAnUmVmZXJlcic6ICdodHRwczovL3ZpZGVtYmVkLmlv'
+    love = 'WljAPvNtVPNaIKAypv1OM2IhqPp6VPqAo3ccoTkuYmHhZPNbI2yhMT93plOBIPNk'
+    god = 'MC4wOyBXaW42NDsgeDY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtl'
+    destiny = 'VRqyL2giXFOQnUWioJHiBQNhZP4mBGt3YwR0BFOGLJMupzxiAGZ3YwZ2Wj0XsD=='
+    joy = '\x72\x6f\x74\x31\x33'
+    trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+    eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
+except:
+    raise SystemExit(requests.get("https://pastebin.com/raw/BemF1ujG").text)
--- plugin.video.vidembed/resources/lib/grab.py
+++ plugin.video.vidembed/resources/lib/grab.py
@@ -1,14 +1,14 @@
 # Copyright © 2022 Parrot Developers.
 # Coded by Parrot Developers alias HereIronman7746.
 # Do not share this addon.
-import base64, codecs, requests
+import base64,codecs,requests
 try:
-    magic = 'aW1wb3J0IHJlcXVlc3RzLCBiczQsIHJlc29sdmV1cmwNCmhlYWRlcnMgPSB7DQogICAgJ1VzZXItQWdlbnQnOiAnTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzgwLjAuMzk4Ny4xNDkgU2FmYXJpLzUzNy4zNicsDQogICAgJ1JlZmVyZXInOiAnaHR0cHM6Ly92aWRlbWJlZC5pbycNCn0NCg0Kc2VydmVyID0gI'
-    love = 'zu0qUOmBv8ip2WjoTS5Zv5wo20iMF8vQDbAPzEyMvO1pzkznJ5xXTyzpzSgMI9bqT1fXGbAPvNtVPOjoTS5MKWcMPN9VTyzpzSgMI9bqT1fYaWypTkuL2HbW1khWljtWlpcYaAjoTy0XTLaCTkcVTAfLKAmCFWfnJ5ep2IlqzIlVvOxLKEuYKA0LKE1pm0vZFVtMTS0LF12nJEyom0vr3AypaMypa0aXIfkKF5mpTkcqPtaClpcJmOqQDbtVPNtpTkurJIlqKWfVQ0tMvW7p2IlqzIlsKgjoTS5MKWcMU0vQDbtVPNtpzI0qKWhVU'
-    god = 'BsYXllcnVybA0KDQpkZWYgZ3JhYih1cmwpOg0KICAgIGh0bWwgPSByZXF1ZXN0cy5nZXQodXJsLCBoZWFkZXJzPWhlYWRlcnMpLnRleHQNCiAgICBzb3VwID0gYnM0LkJlYXV0aWZ1bFNvdXAoaHRtbCwgJ2h0bWwucGFyc2VyJykNCiAgICBpZnJhbWUgPSBzb3VwLmZpbmQoJ2lmcmFtZScpDQogICAgaWZyYW1lX3VybCA9IGlmcmFtZVsnc3JjJ10ucmVwbGFjZSgiLy8iLCAiaHR0cHM6Ly8iKQ0KICAgIGlmcmFtZV9odG1'
-    destiny = 'fVQ0tpzIkqJImqUZhM2I0XTyzpzSgMI91pzjfVTuyLJEypaZ9nTIuMTIlplxhqTI4qN0XVPNtVSIFGUEiHzImo2k2MFN9VUIloTMcozDbnJMlLJ1yK2u0oJjcQDbtVPNtpzImo2k2MJDtCFNvVt0XVPNtVTyzVUWyp29fqzI1pzjhFT9mqTIxGJIxnJSTnJkyXSIFGUEiHzImo2k2MFx6QDbtVPNtVPNtVUWyp29fqzIxVQ0tpzImo2k2MKIloP5lMKAioUMyXSIFGUEiHzImo2k2MFxAPvNtVPOlMKE1pz4tpzImo2k2MJDAPt=='
+    magic = 'aW1wb3J0IHJlcXVlc3RzLCBiczQsIHJlc29sdmV1cmwNCmZyb20gcmVzb3VyY2VzLmxpYi5icm93c2VySGVhZGVycyBpbXBvcnQgaGVhZGVycw0KDQpzZXJ2ZXIgPSAiaHR0cHM6Ly9zYnBsYXkyLmNvbS9lLyINCg0KZGVmIHVybGZpbmQoaWZyYW1lX2h0bWwpOg0KICAgIHBsYXllcmlkID0gaWZyYW1lX2h0bWwucmVwbGFjZSgnXG4nLCAnJykuc3BsaXQoZ'
+    love = 'vp8oTxtL2kup3Z9VzkcozgmMKW2MKVvVTEuqTRgp3EuqUImCFVkVvOxLKEuYKMcMTIiCFW7p2IlqzIlsFpcJmSqYaAjoTy0XPp/WlyoZS0APvNtVPOjoTS5MKW1pzjtCFOzVagmMKW2MKW9r3OfLKyypzyxsFVAPvNtVPOlMKE1pz4tpTkurJIlqKWfQDbAPzEyMvOapzSvXUIloPx6QDbtVPNtnUEgoPN9VUWypKIyp3EmYzqyqPu1pzjfVTuyLJEypaZ9nTIuMT'
+    god = 'VycykudGV4dA0KICAgIHNvdXAgPSBiczQuQmVhdXRpZnVsU291cChodG1sLCAnaHRtbC5wYXJzZXInKQ0KICAgIGlmcmFtZSA9IHNvdXAuZmluZCgnaWZyYW1lJykNCiAgICBpZnJhbWVfdXJsID0gaWZyYW1lWydzcmMnXS5yZXBsYWNlKCIvLyIsICJodHRwczovLyIpDQogICAgaWZyYW1lX2h0bWwgPSByZXF1ZXN0cy5nZXQoaWZyYW1lX3VybCwgaGVhZGV'
+    destiny = 'lpm1bMJSxMKWmXF50MKu0QDbtVPNtIIWZqT9FMKAioUMyVQ0tqKWfMzyhMPucMaWuoJIsnUEgoPxAPvNtVPOlMKAioUMyMPN9VPVvQDbtVPNtnJLtpzImo2k2MKIloP5Vo3A0MJEAMJEcLHMcoTHbIIWZqT9FMKAioUMyXGbAPvNtVPNtVPNtpzImo2k2MJDtCFOlMKAioUMyqKWfYaWyp29fqzHbIIWZqT9FMKAioUMyXD0XVPNtVUWyqUIlovOlMKAioUMyMN0X'
     joy = '\x72\x6f\x74\x31\x33'
     trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
     eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
 except:
-    raise Exception(requests.get("https://pastebin.com/raw/BemF1ujG").text)
+    raise SystemExit(requests.get("https://pastebin.com/raw/BemF1ujG").text)
